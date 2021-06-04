8a5a4b6f9ae8f79515e6fb5d5af0c2473258ff3a
noblue5 <69118557+noblue5@users.noreply.github.com>
2021-06-04 18:02:50 +0100

    Create minechunk.py

---
+++ minechunk.py
@@ -0,0 +1,23 @@
+# Minechunk bot
+# Coded by noblue5
+
+# // Imports \\
+
+import discord
+from discord.ext import commands
+from idlelib import query
+import requests
+
+# // Important stuff \\
+
+client = commands.Bot(command_prefix='m!')
+client.remove_command('help')
+version = "Private testing"
+
+
+# // On_ready Event \\
+
+@client.event
+async def on_ready():
+    print('Minechunk has been started...')
+    await client.change_presence(activity=discord.Game(version))
